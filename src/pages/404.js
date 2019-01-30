d730eaf582df9f8a4fee18ae1cfba087ec02ade4
Louise Davies <louise.davies@stfc.ac.uk>
2019-01-30 16:28:04 +0000

    Add searchbar to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React from "react";
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import Searchbar from "../components/searchbar";
+import css from "@emotion/css";
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>It looks like nothing was found at this location. Maybe try a search?</p>
+    <Searchbar
+      inputCSS={css`
+        width: 300px;
+      `}
+    />
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
