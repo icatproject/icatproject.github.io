4032843ca62f14b44eed8d2643387134462ab09d
Louise Davies <louise.davies@stfc.ac.uk>
2020-08-12 13:37:50 +0100

    Fix search page not re-searching + improve code

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -6,89 +6,63 @@ import { Index } from 'elasticlunr';
 import queryString from 'query-string';
 import SEO from '../components/seo';
 
-export default class SearchPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      query: '',
-      results: [],
-    };
-  }
+function search(index, query) {
+  const results = index
+    // Query the index with search string to get an [] of IDs
+    .search(query, { expand: true })
+    // Map over each ID and return the full document
+    .map(({ ref }) => index.documentStore.getDoc(ref));
 
-  componentDidMount() {
-    const { location } = this.props;
-    const qs = queryString.parse(location.search);
-    const { query } = qs;
-    const results = this.search(query);
-    this.setState({
-      query,
-      results,
-    });
-  }
-
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (nextProps.location.search !== prevState.query) {
-      const qs = queryString.parse(nextProps.location.search);
-      const { query } = qs;
-      return { query };
-    }
-    return null;
-  }
-
-  getOrCreateIndex() {
-    const { data } = this.props;
-    return this.index
-      ? this.index
-      : // Create an elastic lunr index and hydrate with graphql query results
-        Index.load(data.siteSearchIndex.index);
-  }
-
-  search(query) {
-    this.index = this.getOrCreateIndex();
-    const results = this.index
-      // Query the index with search string to get an [] of IDs
-      .search(query, { expand: true })
-      // Map over each ID and return the full document
-      .map(({ ref }) => this.index.documentStore.getDoc(ref));
-
-    return results;
-  }
-
-  render() {
-    const { query, results } = this.state;
-    return (
-      <>
-        <SEO title="Search" keywords={[`search`, `ICAT`]} />
-        <h1>{`Search Results for: ${query}`}</h1>
-        {results.map((page) => (
-          <article key={page.slug}>
-            <h2>
-              <Link
-                css={css`
-                  color: green;
-                `}
-                to={page.slug}
-              >
-                {page.title}
-              </Link>
-            </h2>
-            <p>
-              {page.excerpt}{' '}
-              <Link
-                css={css`
-                  color: green;
-                `}
-                to={page.slug}
-              >
-                Continue reading →
-              </Link>
-            </p>
-          </article>
-        ))}
-      </>
-    );
-  }
+  return results;
 }
+
+const SearchPage = (props) => {
+  const { location, data } = props;
+  const { query } = queryString.parse(location.search);
+  // Create an elastic lunr index and hydrate with graphql query results
+  const index = React.useMemo(() => Index.load(data.siteSearchIndex.index), [
+    data.siteSearchIndex.index,
+  ]);
+  const [results, setResults] = React.useState([]);
+
+  React.useEffect(() => {
+    setResults(search(index, query));
+  }, [index, query]);
+
+  return (
+    <>
+      <SEO title="Search" keywords={[`search`, `ICAT`]} />
+      <h1>{`Search Results for: ${query}`}</h1>
+      {results.map((page) => (
+        <article key={page.slug}>
+          <h2>
+            <Link
+              css={css`
+                color: green;
+              `}
+              to={page.slug}
+            >
+              {page.title}
+            </Link>
+          </h2>
+          <p>
+            {page.excerpt}{' '}
+            <Link
+              css={css`
+                color: green;
+              `}
+              to={page.slug}
+            >
+              Continue reading →
+            </Link>
+          </p>
+        </article>
+      ))}
+    </>
+  );
+};
+
+export default SearchPage;
 
 export const query = graphql`
   query SearchIndexQuery {
